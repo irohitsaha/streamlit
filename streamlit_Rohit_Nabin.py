06691a2db0da886aab86c43cfadbb87a3f32452d
irohitsaha <103872396+irohitsaha@users.noreply.github.com>
2023-06-27 13:07:26 +0530

    Add files via upload

---
+++ streamlit_Rohit_Nabin.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title("Titanic Dataset")
+
+df = pd.read_csv("\\streamlit\\titanic.csv")
+st.dataframe(df)
+
+st.subheader("Description of the dataset")
+st.write(df.describe())
+
+st.subheader("Survived Persons by Sex")
+st.bar_chart(data=df, x="Sex", y="Survived")
+
+st.subheader("Survived persons by Pclass")
+st.bar_chart(data=df, x="Pclass", y="Survived")
+
+st.subheader("Age Distribution")
+age_data = df['Age']
+fig, ax = plt.subplots()
+ax.hist(age_data, bins=20)
+ax.set_xlabel('Age')
+ax.set_ylabel('Count')
+st.pyplot(fig)
+
